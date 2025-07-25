754a35fce9c6a52de5ffaafdcb765dce145aeddb
A.F.FAIZUR RAHMAN <faizur.rahman@brainstation-23.com>
2025-07-25 16:28:05 +0600

    Delete src directory

--- src/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yourcompany.taskmanagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-    // You can add custom queries here if needed
-}
